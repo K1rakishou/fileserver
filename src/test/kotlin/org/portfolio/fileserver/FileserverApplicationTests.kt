325932bf0eef83b9de25e5df037d246314889aa4
kirakishou <kez1911@protonmail.com>
2017-10-19 18:18:09 +0300

    Added properties and tests folder

---
+++ src/test/kotlin/org/portfolio/fileserver/FileserverApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.portfolio.fileserver
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FileserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
